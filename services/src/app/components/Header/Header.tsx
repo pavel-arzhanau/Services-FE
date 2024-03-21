7ae158e1b31c6e980c801fcbcce506c37c9c95b2
Pavel Arzhanov <pavelarzhanau@gmail.com>
2024-03-21 20:29:00 +0300

    feat: header navbar

--- services/src/app/components/Header/Header.tsx
+++ services/src/app/components/Header/Header.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 import styles from "./Header.module.css";
+import Avatar from "@mui/material/Avatar";
+import LanguageIcon from "@mui/icons-material/Language";
+import { useLanguageStore } from "@/app/stores/languageStore";
+import { useEffect, useRef } from "react";
 
 interface HeaderProps {
   isChecked?: boolean;
@@ -7,25 +11,53 @@ interface HeaderProps {
 }
 
 export default function Header({ isChecked, setIsChecked }: HeaderProps) {
+  const { setLanguage } = useLanguageStore((state) => ({
+    setLanguage: state.setLanguage,
+  }));
+
+  const selectRef = useRef<HTMLSelectElement>(null);
+
+  const changeLanguage = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setLanguage(e.target.value);
+    localStorage.setItem("language", e.target.value);
+  };
+
+  useEffect(() => {
+    selectRef.current.value = localStorage.getItem("language") || "ru";
+  }, []);
+
   return (
     <header className={styles.header}>
       <Image src="/logo.png" alt="logo" width={150} height={75} />
       <nav>
         <ul className={styles.list}>
           <li>
-            <a href="/">Home</a>
+            <a href="/">Главная</a>
           </li>
           <li>
-            <a href="/tasks">Tasks</a>
+            <a href="/tasks">Задания</a>
           </li>
           <li>
-            <a href="/favorites">Favorites</a>
+            <a href="/favorites">Избранное</a>
           </li>
           <li>
-            <a href="/about">About</a>
+            <a href="/about">О нас</a>
           </li>
         </ul>
       </nav>
+      <div>
+        <LanguageIcon />
+        <select
+          ref={selectRef}
+          onChange={changeLanguage}
+          name="language"
+          id="language-select"
+        >
+          <option value="ru">Русский</option>
+          <option value="by">Белорусский</option>
+        </select>
+      </div>
+      <Avatar src="/broken-image.jpg" />
     </header>
   );
 }

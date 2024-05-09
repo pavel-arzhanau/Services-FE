f80955c110830a38ae94d50c5d10e94c19c8b0f8
Pavel Arzhanov <pavelarzhanau@gmail.com>
2024-05-09 20:50:16 +0300

    feat: responsive footer

--- services/src/app/components/Footer/Footer.tsx
+++ services/src/app/components/Footer/Footer.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
-import styles from "./Footer.module.css";
+import styles from "./Footer.module.scss";
 import { getDictionary } from "@/app/utils/getDictionary";
 import CustomLink from "../CustomLink/CustomLink";
 import { supportedLanguages } from "@/types";
 import Divider from "@mui/material/Divider";
 import LanguageSwitcher from "../LanguageSwitcher/LanguageSwitcher";
 import LanguageIcon from "@mui/icons-material/Language";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
 
 type Props = {
   lang: supportedLanguages;
@@ -15,38 +18,54 @@ export default async function Footer({ lang }: Props) {
   const dictionary = await getDictionary(lang);
   const menuItems = [
     {
-      path: `/support`,
-      title: dictionary.header.nav.support,
+      path: `/create-task`,
+      title: dictionary.header.nav.createTask,
     },
     {
       path: `/tasks`,
       title: dictionary.header.nav.tasks,
     },
     {
-      path: `/favorites`,
-      title: dictionary.header.nav.favorites,
+      path: `/ads`,
+      title: dictionary.header.nav.ads,
+    },
+    {
+      path: `/responses`,
+      title: dictionary.header.nav.responses,
     },
     {
       path: `/about`,
       title: dictionary.header.nav.about,
+    },
+    {
+      path: `/faq`,
+      title: dictionary.header.nav.faq,
     },
   ];
 
   return (
     <footer className={styles.footerWrapper}>
       <div className={styles.footer}>
-        <CustomLink href={"/"} lang={lang}>
-          <Image src="/logo.png" alt="logo" width={280} height={140} />
-        </CustomLink>
-        <ul className={styles.list}>
-          {menuItems.map((item) => (
-            <li key={item.path}>
-              <CustomLink href={item.path} lang={lang}>
-                {item.title}
-              </CustomLink>
-            </li>
-          ))}
-        </ul>
+        <div className={styles.flexContainer}>
+          <ul className={styles.list}>
+            {menuItems.map((item) => (
+              <li key={item.path}>
+                <CustomLink href={item.path} lang={lang}>
+                  {item.title}
+                </CustomLink>
+              </li>
+            ))}
+          </ul>
+          <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
+            <p style={{ color: "#9ECDF6", margin: 0 }}>По всем вопросам:</p>
+            <p style={{ margin: 0 }}>mockservices@gmail.com</p>
+          </div>
+          <div className={styles.iconsBlock}>
+            <TelegramIcon className={styles.icon} />
+            <InstagramIcon className={styles.icon} />
+            <YouTubeIcon className={styles.icon} />
+          </div>
+        </div>
         <Divider variant="middle" sx={{ width: "90%" }} />
         <div className={styles.basement}>
           <p>© 2024 Services </p>
